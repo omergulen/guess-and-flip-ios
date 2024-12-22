88c64594dab88dd3fa8128bc749edf60439a3e94
Omer Gulen <omrglen@gmail.com>
2024-12-22 18:31:43 +0100

    Initial Commit

---
+++ guess-and-flipTests/guess_and_flipTests.swift
@@ -0,0 +1,17 @@
+//
+//  guess_and_flipTests.swift
+//  guess-and-flipTests
+//
+//  Created by Omer Gulen on 22.12.24.
+//
+
+import Testing
+@testable import guess_and_flip
+
+struct guess_and_flipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ guess-and-flipUITests/guess_and_flipUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  guess_and_flipUITestsLaunchTests.swift
+//  guess-and-flipUITests
+//
+//  Created by Omer Gulen on 22.12.24.
+//
+
+import XCTest
+
+final class guess_and_flipUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
